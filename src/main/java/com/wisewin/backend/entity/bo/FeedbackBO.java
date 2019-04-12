f3f050ef3bcfb5c564d31c5696f0190ca01c6c09
18631323023 <qq492690314>
2019-04-12 15:36:25 +0800

    创建意见反馈

---
+++ src/main/java/com/wisewin/backend/entity/bo/FeedbackBO.java
@@ -0,0 +1,99 @@
+package com.wisewin.backend.entity.bo;
+
+import com.wisewin.backend.common.base.BaseModel;
+
+import java.util.Date;
+import java.util.List;
+
+public class FeedbackBO extends BaseModel{
+    private Integer id;//意见反馈
+    private Integer userid;//用户id
+    private String content;//反馈内容
+    private String image;//图片
+    private String contactway;//联系方式
+    private String contactnumber;//联系号码
+    private Date createtime;//创建时间
+    private String status;//状态
+    private Integer adminid;//操作员id
+    private Date updatetime;//修改时间
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getContactway() {
+        return contactway;
+    }
+
+    public void setContactway(String contactway) {
+        this.contactway = contactway;
+    }
+
+    public String getContactnumber() {
+        return contactnumber;
+    }
+
+    public void setContactnumber(String contactnumber) {
+        this.contactnumber = contactnumber;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getAdminid() {
+        return adminid;
+    }
+
+    public void setAdminid(Integer adminid) {
+        this.adminid = adminid;
+    }
+
+    public Date getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(Date updatetime) {
+        this.updatetime = updatetime;
+    }
+}
