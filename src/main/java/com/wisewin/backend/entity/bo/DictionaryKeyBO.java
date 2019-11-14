9fc52f3f7098f1db6c92fe775d02636b309dee26
wangyang <18731612878@163.com>
2019-11-14 10:42:27 +0800

    虚拟用户修改

--- src/main/java/com/wisewin/backend/entity/bo/DictionaryKeyBO.java
+++ src/main/java/com/wisewin/backend/entity/bo/DictionaryKeyBO.java
@@ -6,6 +6,14 @@ public class DictionaryKeyBO {
     private Integer id;
     private String key;
 
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
     public Integer getId() {
         return id;
     }
